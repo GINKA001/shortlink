9c243d5b6e3d3e1837c08f664a92244b5d45b9b1
GINKA001 <3515313481@qq.com>
2026-04-07 10:04:35 +0800

    有效期时间序列化

--- project/src/main/java/com/ginka/shortlink/shortlink/project/dto/req/ShortLinkCreateReqDTO.java
+++ project/src/main/java/com/ginka/shortlink/shortlink/project/dto/req/ShortLinkCreateReqDTO.java
@@ -1,6 +1,7 @@
 package com.ginka.shortlink.shortlink.project.dto.req;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -26,16 +27,17 @@ public class ShortLinkCreateReqDTO {
     /**
      * 创建类型 0：控制台 1：接口
      */
-    private int createdType;
+    private Integer createdType;
 
     /**
      * 有效期类型 0：永久有效 1：用户自定义
      */
-    private int validDateType;
+    private Integer validDateType;
 
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate;
 
     /**
