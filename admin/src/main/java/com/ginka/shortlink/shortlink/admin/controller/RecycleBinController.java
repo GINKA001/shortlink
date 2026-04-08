279cf161906b669383cb7944c24708d97db55ff0
GINKA001 <3515313481@qq.com>
2026-04-08 18:06:14 +0800

    开发短链接新增回收站功能

---
+++ admin/src/main/java/com/ginka/shortlink/shortlink/admin/controller/RecycleBinController.java
@@ -0,0 +1,26 @@
+package com.ginka.shortlink.shortlink.admin.controller;
+
+
+import com.ginka.shortlink.shortlink.admin.common.convention.result.Result;
+import com.ginka.shortlink.shortlink.admin.common.convention.result.Results;
+import com.ginka.shortlink.shortlink.admin.remote.dto.ShortLinkRemoteService;
+import com.ginka.shortlink.shortlink.admin.remote.dto.req.RecycleBinSaveReqDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 回收站控制层
+ */
+@RestController
+@RequiredArgsConstructor
+public class RecycleBinController {
+    ShortLinkRemoteService shortLinkRemoteService=new ShortLinkRemoteService(){};
+    @PostMapping("/api/short-link/admin/v1/recycle-bin/save")
+    public Result<Void> saveRecycleBin(@RequestBody RecycleBinSaveReqDTO requestParam){
+        shortLinkRemoteService.saveRecycleBin(requestParam);
+        return Results.success();
+    }
+
+}
