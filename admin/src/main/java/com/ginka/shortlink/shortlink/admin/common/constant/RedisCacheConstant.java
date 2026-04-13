ac396cd0245a01386b3a4140eaebb97681f3ad35
GINKA001 <3515313481@qq.com>
2026-04-13 13:44:01 +0800

    优化用户创建分组数量不能大于20

--- admin/src/main/java/com/ginka/shortlink/shortlink/admin/common/constant/RedisCacheConstant.java
+++ admin/src/main/java/com/ginka/shortlink/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -4,5 +4,9 @@ package com.ginka.shortlink.shortlink.admin.common.constant;
  * Redis缓存常量
  */
 public class RedisCacheConstant {
-    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register";
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+    /**
+     * 分组创建分布式锁
+     */
+    public static final String LOCK_GROUP_CREATE_KEY = "short-link:lock_group-create:%s";
 }
--- admin/src/main/java/com/ginka/shortlink/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/ginka/shortlink/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ginka.shortlink.shortlink.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -16,22 +17,30 @@ import com.ginka.shortlink.shortlink.admin.remote.dto.ShortLinkRemoteService;
 import com.ginka.shortlink.shortlink.admin.remote.dto.resp.ShortLinkCountQueryRespDTO;
 import com.ginka.shortlink.shortlink.admin.service.GroupService;
 import com.ginka.shortlink.shortlink.admin.util.RandomCodeUtils;
-import lombok.Builder;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+
+import static com.ginka.shortlink.shortlink.admin.common.constant.RedisCacheConstant.LOCK_GROUP_CREATE_KEY;
 
 /**
  * 短链接分组服务实现层
  */
 @Service
 @Slf4j
-
+@RequiredArgsConstructor
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
     ShortLinkRemoteService shortLinkRemoteService=new ShortLinkRemoteService(){};
+    private  final RedissonClient redissonClient;
+
+    @Value("${short-link.group.max-num}")
+    private Integer maxGroupSize;
 
     @Override
     public void saveGroup(String groupName){
@@ -39,23 +48,36 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
     }
     @Override
     public void saveGroup(String username,String groupName) {
-        String gid="";
-        while(true) {
-            gid = RandomCodeUtils.generateSecureRandomCode();
-            LambdaQueryWrapper<GroupDO> eq = Wrappers.lambdaQuery(GroupDO.class)
-                    .eq(GroupDO::getGid, gid)
-                    // 设置用户名
-                    .eq(GroupDO::getUsername,username);
-            if (baseMapper.selectOne(eq) == null) {
-                break;
+        RLock lock = redissonClient.getLock(String.format(LOCK_GROUP_CREATE_KEY, username));
+        lock.lock();
+        try {
+            String gid="";
+            LambdaQueryWrapper<GroupDO> eq1 = Wrappers.lambdaQuery(GroupDO.class).eq(GroupDO::getUsername, username)
+                    .eq(GroupDO::getDelFlag, 0);
+            List<GroupDO> groupDOS = baseMapper.selectList(eq1);
+            if(CollUtil.isNotEmpty(groupDOS) && groupDOS.size() == maxGroupSize){
+                throw new ClientException(String.format("超出最大分组数:%d",maxGroupSize));
             }
+            while(true) {
+                gid = RandomCodeUtils.generateSecureRandomCode();
+                LambdaQueryWrapper<GroupDO> eq = Wrappers.lambdaQuery(GroupDO.class)
+                        .eq(GroupDO::getGid, gid)
+                        // 设置用户名
+                        .eq(GroupDO::getUsername,username);
+                if (baseMapper.selectOne(eq) == null) {
+                    break;
+                }
+            }
+            GroupDO build = GroupDO.builder()
+                    .username(UserContext.getUsername())
+                    .gid(gid)
+                    .name(groupName)
+                    .build();
+            baseMapper.insert(build);
+        }finally {
+            lock.unlock();
         }
-        GroupDO build = GroupDO.builder()
-                .username(UserContext.getUsername())
-                .gid(gid)
-                .name(groupName)
-                .build();
-        baseMapper.insert(build);
+
     }
 
     @Override
