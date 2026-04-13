8728e084810947872cb6c018a2a9053a5ae04a21
GINKA001 <3515313481@qq.com>
2026-04-13 10:29:29 +0800

    fix: 修复短链接恢复有效期后无法访问的问题

--- project/src/main/java/com/ginka/shortlink/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/ginka/shortlink/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -185,6 +185,15 @@ public class ShortLinkServiceImpl extends ServiceImpl<LinkMapper, ShortLinkDO> i
         baseMapper.delete(eq);
         baseMapper.insert(build1);
 
+        if(Objects.equals(shortLinkDO.getValidDateType(),requestParam.getValidDateType()) || Objects.equals(shortLinkDO.getValidDate(),requestParam.getValidDate())){
+            stringRedisTemplate.delete(String.format(RedisKeyConstant.GOTO_SHORT_LINK_KEY, shortLinkDO.getFullShortUrl()));
+            if (shortLinkDO.getValidDate() != null && shortLinkDO.getValidDate().before(new Date())){
+                if (shortLinkDO.getValidDateType() == VailDateTypeEnum.PERMANENT.getType() || shortLinkDO.getValidDate().after(new Date())){
+                    stringRedisTemplate.delete(String.format(RedisKeyConstant.GOTO_IS_NULL_SHORT_LINK_KEY, shortLinkDO.getFullShortUrl()));
+                }
+            }
+        }
+
     }
 
     @SneakyThrows
@@ -235,7 +244,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<LinkMapper, ShortLinkDO> i
                     .eq(ShortLinkDO::getEnableStatus, 0)
                     .eq(ShortLinkDO::getDelFlag, 0);
             ShortLinkDO shortLinkDO = baseMapper.selectOne(eq);
-            if (shortLinkDO == null || shortLinkDO.getValidDate().before(new Date())) {
+            if (shortLinkDO == null || (shortLinkDO.getValidDate() !=null && shortLinkDO.getValidDate().before(new Date()))) {
                 stringRedisTemplate.opsForValue().set(String.format(RedisKeyConstant.GOTO_IS_NULL_SHORT_LINK_KEY, fullShortUrl), "-",30L, TimeUnit.MINUTES);
                 ((HttpServletResponse) response).sendRedirect("/page/notfound");
                 return;
